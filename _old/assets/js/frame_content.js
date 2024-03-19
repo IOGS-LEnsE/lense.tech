0341158c610a346bffc84a70223f8c7b0c7ec4ba
Julien Villemejane <julien.villemejane@institutoptique.fr>
2024-03-19 08:44:54 +0100

    Remove old version

--- _old/assets/js/frame_content.js
+++
@@ -1,7 +0,0 @@
-
-function sendToFrame(message) {
-	var iframe = document.getElementById("frame-content");
-	if (iframe && iframe.contentWindow) {
-		iframe.contentWindow.postMessage(message, "*");
-	}
-}
